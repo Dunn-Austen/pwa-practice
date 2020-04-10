7fb2be3e308be5d3f1c40305805912831ef80132
Alan Turing <austeyd@gmail.com>
2020-04-09 23:38:22 -0600

    Strip needless code from manifest file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <main className="App">
       <div>
         <p>Placeholder text</p>
-        <img alt='image of a pokeball' src='./public/pokeball-512.png' />
+        <img alt='image of a pokeball' src='https://image.shutterstock.com/image-vector/september-23-2016-vector-icon-260nw-487367941.jpg' />
       </div>
     </main>
   );
